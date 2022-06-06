ec2b6717a4daa69d80b336d251c4272b3b900dc5
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-06-06 20:08:18 +0300

    commit tree ff870245fd8170484c3e853695c14c72a5993767

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor( Math.random() * 100 ) + 1;
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(3, 5, 7,7,3,20,19,3,5,2);
+console.log(max);
+
+
+Math.random();
+console.log(Math.floor(Math.random()*10)+1);
